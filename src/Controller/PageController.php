e8bc6e717f8e5a105a1a85d9e7f185c0a45ec8e2
Michael Rodin <mikhail.rodin@satellite-soft.ru>
2021-06-08 22:54:47 +0400

    Added page controller

---
+++ src/Controller/PageController.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller that handles general app templates and builds primary visual environment.
+ *
+ * Class PageController
+ * @package App\Controller
+ *
+ * @Route(name="page.")
+ */
+class PageController extends AbstractController
+{
+
+    /**
+     * @Route("", methods={"GET"})
+     * @param Request $request
+     * @return Response|null
+     */
+    public function startPage(Request $request)
+    {
+        return new RedirectResponse('home');
+    }
+
+    /**
+     * @Route("home", name="home", methods={"GET"})
+     * @param Request $request
+     * @return Response|null
+     */
+    public function homePage(Request $request)
+    {
+        return $this->render('pages/home/home.html.twig', [
+            'pagetitle' => 'Домашняя'
+        ]);
+    }
+
+    /**
+     * @Route("about", name="about", methods={"GET"})
+     * @param Request $request
+     * @return Response|null
+     */
+    public function aboutPage(Request $request)
+    {
+        return $this->render('pages/about/about.html.twig', [
+            'pagetitle' => 'О нас'
+        ]);
+    }
+
+    /**
+     * @Route("disciplines", name="disciplines", methods={"GET"})
+     * @param Request $request
+     * @return Response|null
+     */
+    public function disciplinesPage(Request $request)
+    {
+        return $this->render('pages/disciplines/disciplines.html.twig', [
+            'pagetitle' => 'Предметы'
+        ]);
+    }
+
+    /**
+     * @Route("tests", name="tests", methods={"GET"})
+     * @param Request $request
+     * @return Response|null
+     */
+    public function testsPage(Request $request)
+    {
+        return $this->render('pages/tests/tests.html.twig', [
+            'pagetitle' => 'Тесты'
+        ]);
+    }
+
+}
